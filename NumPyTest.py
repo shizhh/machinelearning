097cb82162d15761c34e82fa151a9ada0a5dc462
shizhh <shizhh126@126.com>
2015-12-13 19:01:46 +0800

    knn

---
+++ NumPyTest.py
@@ -0,0 +1,12 @@
+# coding : utf-8
+
+from numpy import *
+array = random.rand(4,4)
+print array
+randMat = mat(array)
+print randMat
+invrandMat = randMat.I
+I = randMat*invrandMat
+print I
+myeye = I - eye(4)
+print myeye
